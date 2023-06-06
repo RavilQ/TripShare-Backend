2d18feab2cfa3b91094df3605b454218f2cca199
Ravil <ravil.quliyev.98@bk.ru>
2023-06-06 20:39:11 +0400

    Hell eledim closes #1

---
+++ ApiPractice.Core/Entities/Trip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPractice.Core.Entities
+{
+    public class Trip:BaseEntity
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
