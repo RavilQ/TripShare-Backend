c675290d6352f236e470105bf4780499776dbc67
Ravil <ravil.quliyev.98@bk.ru>
2023-06-12 17:29:57 +0400

    End The Identity

---
+++ ApiPracitce/Dtos/AccountDtos/LoginDto.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace ApiPracitce.Dtos.AccountDtos
+{
+    public class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.UserName).NotNull().NotEmpty().MaximumLength(25).MinimumLength(5);
+            RuleFor(x => x.Password).NotNull().NotEmpty().MaximumLength(25).MinimumLength(5);
+        }
+    }
+}
